ed08cc2be2200149d5d04c9cfe37f6d72f4eb342
Shun Kushigami <s061064@gmail.com>
2017-06-21 09:46:08 +0900

    upgrade jest to v20.0.4

--- __tests__/app/native/components/instructions.js
+++ __tests__/app/native/components/instructions.js
@@ -2,12 +2,13 @@ import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import {Provider} from 'react-intl-redux';
+import {MemoryRouter} from 'react-router-native';
 
 import {store} from '../../../../src/app/native/containers/app';
 import Instructions from '../../../../src/app/native/components/instructions';
 
 describe('ios instructions component', () => {
     it('should be rendered correctly', () => {
-        const tree = renderer.create(<Provider store={store}><Instructions/></Provider>);
+        const tree = renderer.create(<Provider store={store}><MemoryRouter><Instructions/></MemoryRouter></Provider>);
     });
 });
--- __tests__/app/web/containers/index.js
+++ __tests__/app/web/containers/index.js
@@ -2,12 +2,13 @@ import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import {Provider} from 'react-intl-redux';
+import {MemoryRouter} from 'react-router-dom';
 
 import {store} from '../../../../src/app/web/containers/app';
 import Index from '../../../../src/app/web/containers/index';
 
 describe('web index container', () => {
     it('should be rendered correctly', () => {
-        const tree = renderer.create(<Provider store={store}><Index/></Provider>);
+        const tree = renderer.create(<Provider store={store}><MemoryRouter><Index/></MemoryRouter></Provider>);
     });
 });
